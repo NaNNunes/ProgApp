94e4b111f7bd932d900ae8c672fd5d5873118167
RENAN DE SOUZA NUNES <r.nunes@labcetec.net>
2024-08-09 21:25:28 -0300

    Exercicio for

---
+++ Exercicio_for_counter/src/main/java/com/mycompany/exercicio_for_counter/Exercicio_for_counter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio_for_counter;
+
+/**
+ *
+ * @author r.nunes
+ */
+public class Exercicio_for_counter {
+
+    public static void main(String[] args) {
+        // a --- 1 a 10
+        for (int i = 0; i <= 10; i++ ){
+            System.out.println(i);
+        }
+        // a --- 10 a 1
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
