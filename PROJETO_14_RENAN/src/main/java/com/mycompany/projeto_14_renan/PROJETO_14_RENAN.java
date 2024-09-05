b901a3752567d6272c0ace2308be100c5a38b066
Renan Nunes <renan.s.nunes@aluno.senai.br>
2024-09-05 16:25:05 -0300

    projeto 14, precisando de mostrar valores que repetem

--- PROJETO_14_RENAN/src/main/java/com/mycompany/projeto_14_renan/PROJETO_14_RENAN.java
+++ PROJETO_14_RENAN/src/main/java/com/mycompany/projeto_14_renan/PROJETO_14_RENAN.java
@@ -4,24 +4,43 @@
 
 package com.mycompany.projeto_14_renan;
 
+import java.util.Random;
 import java.io.DataInputStream;
 import java.io.IOException;
 
 /**
- *  
+ *  Escreva um programa que leia uma matriz 5x5 de números inteiros inseridos pelo usuário.
+ *  O programa deve realizar as seguintes operações:
+
+    Exibir a matriz original inserida pelo usuário. v
+    Ordenar a matriz de forma crescente, como se fosse um vetor único, e exibir a matriz ordenada. v
+    Calcular a soma de cada linha e cada coluna da matriz, e exibir essas somas. v
+    Encontrar os números que aparecem mais de uma vez na matriz e exibir quais são e quantas vezes eles aparecem. 
+    Calcular a soma da diagonal principal e da diagonal secundária da matriz.
+    Substituir todos os números primos da matriz por -1 e exibir a nova matriz
  * @author r.nunes
  */
 public class PROJETO_14_RENAN {
     public static DataInputStream data;
-    public static final int tam = 2;
+    public static int entrada;
+    
+    public static final int tam = 5;
     public static int[][] array = new int[tam][tam];
-    public static String entrada;
+    public static int[] vet = new int[tam * tam];
+    
+    public static Random random = new Random();
+    public static final int lim = 11;
     
     public static void main(String[] args) throws IOException{
         InputNum();
+        System.out.println("Matriz de valores: ");
         OutputNum();
         sortArray();
+        System.out.println("Matriz organizada: ");
+        OutputNum();
         somador();
+        System.out.println("\n");
+        EncontraRepetidos();
     }
     // entrada de numeros
     public static void InputNum() throws IOException{
@@ -29,8 +48,8 @@ public class PROJETO_14_RENAN {
             for (int c = 0; c < tam; c++) {
                 System.out.printf("Informe o %d.o valor da %d.a linha: ", c+1, l+1);
                 data = new DataInputStream(System.in);
-                entrada = data.readLine();
-                array[l][c] = Integer.parseInt(entrada);
+                entrada = random.nextInt(lim);
+                array[l][c] = entrada;//Integer.parseInt(entrada);
             }
         }
     }
@@ -38,7 +57,7 @@ public class PROJETO_14_RENAN {
     public static void OutputNum(){
         for(int[] line : array){
             for (int num : line){
-                System.out.printf("%d, ", num);
+                System.out.printf("%2d, ", num);
             }
             System.out.println("");
         }
@@ -46,7 +65,7 @@ public class PROJETO_14_RENAN {
     }
     
     public static void sortArray(){
-        int[] vet = new int[tam * tam];
+        
         int counter = 0;
         // transferindo valores
         for (int l = 0; l < tam; l++){
@@ -55,11 +74,6 @@ public class PROJETO_14_RENAN {
                 counter ++;
             }
         }
-        // mostra array
-        for (int i = 0; i < vet.length; i++) {
-            System.out.printf("%d, ",vet[i]);
-        }
-        System.out.println("");
         
         //sorter
         for (int i = 0; i < vet.length; i++) {
@@ -74,30 +88,63 @@ public class PROJETO_14_RENAN {
             vet[org] = aux;
         }
         
-        // mostra array
-        for (int i = 0; i < vet.length; i++) {
-            System.out.printf("%d, ",vet[i]);
+        // passando vet para array
+        counter = 0;
+        for (int l = 0; l < tam; l++) {
+            for (int c = 0; c < tam; c++) {
+                array[l][c] = vet[counter];
+                counter ++;
+            }
         }
-        System.out.println("");
     }
     
     //soma linha e coluna
     public static void somador(){
-        int[] sLine = new int[tam];
-        int[] sCol = new int[tam];
+        int[] somaLine = new int[tam];
+        int[] somaCol = new int[tam];
+        int counter;
         
-        for (int i = 0; i < tam; i++) {
-            for (int l = 0; l < tam; l++) {
-               for (int c = 0; c < tam; c++) {
-                   sLine[i] += array[c][0];
-               } 
+        //soma coluna
+        counter = 0;
+        for (int l = 0; l < tam; l++) {
+            for (int c = 0; c < tam; c++) {
+                somaCol[counter] += array[c][l];
+            }
+            if (counter < tam){
+                counter ++;
+            }
+        }
+        //soma linha
+        counter = 0;
+        for (int l = 0; l < tam; l++) {
+            for (int c = 0; c < tam; c++) {
+                somaLine[counter] += array[l][c];
+            }
+            if (counter < tam){
+                counter ++;
             }
         }
         
+        System.out.println("Soma de cada coluna: ");
+        for (int i = 0; i < tam; i++){
+            System.out.printf("%d, ", somaCol[i]);
+        }
         System.out.println("");
-        for (int i = 0; i < tam; i++) {
-            System.out.printf("%d, ", sLine[i]);
-        } 
+        System.out.println("Soma de cada linha: ");
+        for (int i = 0; i < tam; i++){
+            System.out.printf("%d, ", somaLine[i]);
+        }
+    }
+    
+    public static void EncontraRepetidos(){
+        int index = 0;
+        int aux = 0;
+        int[] vetAux = vet;
+        
+        int valorVerificado = vetAux[0];
+        for (int i = 0; i < vetAux.length; i++){
+            
+        }
     }
 }
 
