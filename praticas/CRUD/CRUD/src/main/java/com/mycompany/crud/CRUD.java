7c100cfeca277140a2f6b562bdb7f50af5586940
Renan <renan.s.nunes@aluno.senai.br>
2024-10-09 20:49:49 -0300

    crude

---
+++ praticas/CRUD/CRUD/src/main/java/com/mycompany/crud/CRUD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud;
+
+/**
+ *
+ * @author r.nunes
+ */
+public class CRUD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
